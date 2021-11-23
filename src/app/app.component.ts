02b33fc0c0022d92988a26916b0b7d1128eb89bd
JJ240 <tundersro@gmail.com>
2021-11-23 16:54:04 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'BS2';
+  n1:number = 5;
+  n2:number =2;
+  picName:string ="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/322868_1100-800x825.jpg"; 
+
+  hi(name:string){
+    alert("Hi " + name)
+  }
 }
+
